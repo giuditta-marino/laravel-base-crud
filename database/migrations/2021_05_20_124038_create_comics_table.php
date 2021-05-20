8a9da783315595acee8e03535728d28081425896
Giuditta <giudittamarino89@gmail.com>
2021-05-20 15:08:47 +0200

    creato seeder

---
+++ database/migrations/2021_05_20_124038_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('description')->nullable();
+            $table->text('thumb');
+            $table->double('price', 9, 2);
+            $table->text('series');
+            $table->date('sale_date')->nullable();
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
